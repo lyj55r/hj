9c9d92a7f931df6d30eb243aa63f4734590ff911
veip007 <12257917+veip007@users.noreply.github.com>
2019-11-23 09:54:17 +0800

    Update hj.sh

--- hj.sh
+++ hj.sh
@@ -9,19 +9,23 @@ NETMASK="$(( (value >> 24) & 0xff )).$(( (value >> 16) & 0xff )).$(( (value >> 8
 
 
 clear
+while :
+do
 echo "                                                           "
-echo "###########################################################"
-echo "#                                                         #"
-echo "#                      懒人专用                           #"
-echo "#                                                         #"
-echo "#  加速系列：Bbr系列、KCPtun、锐速                        #"
-echo "#  代理系列：V2ray、ssr、Tg专用代理、Goflyway             #"
-echo "#  性能测试：superbench、回程线路测试                     #"
-echo "#  监控系列：云监控（status）                             #"
-echo "#  DD系列：傻瓜式一键DD包                                 #"
-echo "#  脚本来源于网络，版权归各位所有                         #"
-echo "#                                                         #"
-echo "###########################################################"
+echo "+---------------------------------------------------------+" &&
+echo "|                                                         |" &&
+echo "|                      懒人专用                           |" &&
+echo "|                                                         |" &&
+echo "|  加速系列：Bbr系列、KCPtun、锐速                        |" &&
+echo "|  代理系列：V2ray、ssr、Tg专用代理、Goflyway             |" &&
+echo "|  性能测试：superbench、回程线路测试                     |" &&
+echo "|  监控系列：云监控（status）                             |" &&
+echo "|  DD系列：傻瓜式一键DD包                                 |" &&
+echo "|  脚本来源于网络，版权归各位所有                         |" &&
+echo "|                                                         |" &&
+echo "+---------------------------------------------------------+" &&
+check_environment
+analyze_environment
 echo "                                                           "
 echo "请选择您需要的程序:"
 echo "  1) 加速系列：Bbr系列、锐速"
@@ -40,19 +44,19 @@ echo ""
 echo -n "请输入编号: "
 read N
 case $N in
-  1) wget -N --no-check-certificate https://raw)githubusercontent)com/veip007/Linux-NetSpeed/master/tcp)sh && chmod +x tcp)sh && bash tcp)sh ;;
-  2) wget -N --no-check-certificate https://raw)githubusercontent)com/veip007/bbr2)sh/master/bbr2)sh && chmod +x bbr2)sh && bash bbr2)sh ;;
-  3) wget -N --no-check-certificate https://github)com/veip007/Kcptun/raw/master/kcptun/kcptun)sh && chmod +x kcptun)sh && bash kcptun)sh ;;
-  4) wget -N --no-check-certificate https://raw)githubusercontent)com/veip007/doubi/master/ssrmu)sh && chmod +x ssrmu)sh && bash ssrmu)sh ;;
-  5) wget -N --no-check-certificate https://raw)githubusercontent)com/veip007/v2ray/master/v2)sh && chmod +x v2)sh && bash v2)sh ;;
-  6) wget -N --no-check-certificate https://raw)githubusercontent)com/veip007/doubi/master/mtproxy_go)sh && chmod +x mtproxy_go)sh && bash mtproxy_go)sh ;;
-  7) wget -N --no-check-certificate https://git)io/goflyway)sh && chmod +x goflyway)sh && bash goflyway)sh ;;
-  8) wget -N --no-check-certificate https://raw)githubusercontent)com/veip007/cesu/master/superbench)sh && chmod +x superbench)sh && bash superbench)sh ;;
-  9) wget -N --no-check-certificate https://raw)githubusercontent)com/veip007/huicheng/master/huicheng && chmod +x huicheng ;;
-  10) wget -N --no-check-certificate https://raw)githubusercontent)com/veip007/doubi/master/status)sh && chmod +x status)sh && bash status)sh ;;
-  11) wget -N --no-check-certificate https://raw)githubusercontent)com/veip007/dd/master/dd-od)sh && chmod +x dd-od)sh  && )/dd-od)sh
+  1) wget -N --no-check-certificate https://raw.githubusercontent.com/veip007/Linux-NetSpeed/master/tcp.sh && chmod +x tcp.sh && bash tcp.sh ;;
+  2) wget -N --no-check-certificate https://raw.githubusercontent.com/veip007/bbr2.sh/master/bbr2.sh && chmod +x bbr2.sh && bash bbr2.sh ;;
+  3) wget -N --no-check-certificate https://github.com/veip007/Kcptun/raw/master/kcptun/kcptun.sh && chmod +x kcptun.sh && bash kcptun.sh ;;
+  4) wget -N --no-check-certificate https://raw.githubusercontent.com/veip007/doubi/master/ssrmu.sh && chmod +x ssrmu.sh && bash ssrmu.sh ;;
+  5) wget -N --no-check-certificate https://raw.githubusercontent.com/veip007/v2ray/master/v2.sh && chmod +x v2.sh && bash v2.sh ;;
+  6) wget -N --no-check-certificate https://raw.githubusercontent.com/veip007/doubi/master/mtproxy_go.sh && chmod +x mtproxy_go.sh && bash mtproxy_go.sh ;;
+  7) wget -N --no-check-certificate https://git.io/goflyway.sh && chmod +x goflyway.sh && bash goflyway.sh ;;
+  8) wget -N --no-check-certificate https://raw.githubusercontent.com/veip007/cesu/master/superbench.sh && chmod +x superbench.sh && bash superbench.sh ;;
+  9) wget -N --no-check-certificate https://raw.githubusercontent.com/veip007/huicheng/master/huicheng && chmod +x huicheng ;;
+  10) wget -N --no-check-certificate https://raw.githubusercontent.com/veip007/doubi/master/status.sh && chmod +x status.sh && bash status.sh ;;
+  11) wget -N --no-check-certificate https://raw.githubusercontent.com/veip007/dd/master/dd-od.sh && chmod +x dd-od.sh  && )/dd-od.sh
  ;;
-  12) wget -N --no-check-certificate https://raw)githubusercontent)com/veip007/dd/master/dd-gd)sh && chmod +x dd-gd)sh  && )/dd-gd)sh
+  12) wget -N --no-check-certificate https://raw.githubusercontent.com/veip007/dd/master/dd-gd.sh && chmod +x dd-gd.sh  && )/dd-gd.sh
  ;;
   *) echo "Wrong input!" ;;
 esac
